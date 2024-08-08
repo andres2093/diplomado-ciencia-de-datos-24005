b8f9d3a31e14d814cdde71383b5bef83bac1bd96
Andrés Ramírez <andres200493@gmail.com>
2024-08-07 20:26:14 -0600

    S1 -> E1

---
+++ S1/s1.sql
@@ -0,0 +1,9 @@
+-- ///////////////// EJEMPLO 01 /////////////////
+-- Mostrar bases de datos
+show databases;
+-- Seleccionar base de datos "tienda"
+use tienda;
+-- Mostrar tablas
+show tables;
+-- Mostrar estructura de una tabla
+describe Pedidos;
