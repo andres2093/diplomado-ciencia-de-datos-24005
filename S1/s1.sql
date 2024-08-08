1b8bd04f8a9a666c7a3cc198f2b0121419927898
Andrés Ramírez <andres200493@gmail.com>
2024-08-07 21:17:37 -0600

    S1 -> E3

--- S1/s1.sql
+++ S1/s1.sql
@@ -19,4 +19,15 @@ select * from Usuarios;
 -- ///////////////// RETO 02 ///////////////// 
 select * from Pedidos;
 select * from Productos;
+-- ///////////////// EJEMPLO 03 /////////////////
+-- Mostrar todas las columnas de una tabla pero aplicando un filtro
+select * from Usuarios where edad > 30;
+select * from Productos where precio < 100;
+select * from Pedidos where (fecha_pedido >= "2023-01-01" and fecha_pedido <= "2023-12-31") and total_pedido > 100;
+select * from Usuarios where edad > 18 or edad < 65;
+select * from Productos where not stock_disponible > 0;
+
+
+
+
 
