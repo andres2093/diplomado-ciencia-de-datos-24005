6d62659350b06371d67b4e381476cefda5063a35
Andrés Ramírez <andres200493@gmail.com>
2024-08-07 20:30:54 -0600

    S1 -> R1

--- S1/s1.sql
+++ S1/s1.sql
@@ -6,4 +6,8 @@ use tienda;
 -- Mostrar tablas
 show tables;
 -- Mostrar estructura de una tabla
+describe Usuarios;
+-- ///////////////// RETO 01 /////////////////
 describe Pedidos;
+describe Productos;
+describe Detalles_Pedido;
