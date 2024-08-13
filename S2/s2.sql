6ae8065f59e53de975344ade69822d42cd291e42
Andrés Ramírez <andres200493@gmail.com>
2024-08-12 19:44:40 -0600

    S2 -> R1

--- S2/s2.sql
+++ S2/s2.sql
@@ -12,3 +12,15 @@ select MAX(precio) as precio_maximo_producto from Productos;
 select MIN(precio) as precio_maximo_producto from Productos;
 -- AVG
 select AVG(edad) as promedio_edad_usuarios from Usuarios;
+
+-- ///////////////// RETO 01 /////////////////
+-- Calcular la suma total de los subtotales de todos los pedidos.
+SELECT SUM(subtotal) AS subtotal_pedido FROM Detalles_Pedido;
+-- Precio promedio de los productos en stock.
+SELECT AVG(precio) AS precio_promedio FROM Productos WHERE stock_disponible > 0;
+-- Número total de productos vendidos.
+SELECT SUM(cantidad) AS productos_vendidos FROM Detalles_Pedido;
+-- Número de pedidos del usuario 10.
+SELECT COUNT(*) AS numero_pedidos FROM Pedidos WHERE user_id = 10;
+-- Precio máximo de pedidos hechos el 29 de mayo de 2023. 
+SELECT MAX(total_pedido) AS precio_máximo FROM Pedidos WHERE fecha_pedido LIKE '2023-05-29';
