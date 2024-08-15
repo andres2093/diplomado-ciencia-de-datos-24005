d754ccb289948da0691aa50689172734e5ca0ecb
Andrés Ramírez <andres200493@gmail.com>
2024-08-14 20:05:47 -0600

    S3 -> R1

--- S3/s3.sql
+++ S3/s3.sql
@@ -5,3 +5,14 @@ use tienda;
 select u.nombre, u.correo_electronico, (
 	select count(*) from Pedidos p where p.user_id = u.user_id
 ) as total_pedidos from Usuarios u;
+
+-- ///////////////// RETO 01 /////////////////
+-- Mostrar el nombre de cada producto junto con su precio unitario y la cantidad total vendida de ese producto en todos los pedidos
+select p.producto_id, p.nombre_producto, (
+	select MAX(dp.precio_unitario) from Detalles_Pedido dp where dp.producto_id = p.producto_id
+) as precio_unitario_maximo, (
+	select SUM(dp.cantidad) from Detalles_Pedido dp where dp.producto_id = p.producto_id
+) as cantidad_total_vendida from Productos p;
+-- Comprobar
+select MAX(dp.precio_unitario) as precio_unitario_maximo from Detalles_Pedido dp where dp.producto_id = 3;
+select SUM(dp.cantidad) as cantidad_total_vendida from Detalles_Pedido dp where dp.producto_id = 3;
