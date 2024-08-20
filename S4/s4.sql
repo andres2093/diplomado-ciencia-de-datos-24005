ab9f851e858ddf7e1c0de541b0c8a87a44bedb83
Andrés Ramírez <andres200493@gmail.com>
2024-08-19 20:06:23 -0600

    S4 -> E2

--- S4/s4.sql
+++ S4/s4.sql
@@ -8,9 +8,7 @@ u.nombre, u.apellido, p.fecha_pedido, p.total_pedido
 from Usuarios u
 -- INNER JOIN Pedidos p -- INNER es opcional
 JOIN Pedidos p
-JOIN Detalles_Pedidos dp
-ON u.user_id = p.user_id
-ON p.user_id = dp.user_id;
+ON u.user_id = p.user_id;
 
 -- ///////////////// RETO 01 /////////////////
 -- Realiza un análisis de los productos comprados por cada usuario
@@ -28,3 +26,21 @@ ORDER BY u.nombre;
 select * from Detalles_Pedido where pedido_id in(
 	select pedido_id from Pedidos where user_id = 10000
 );
+
+-- ///////////////// EJEMPLO 02 /////////////////
+-- Realiza un análisis de los pedidos y los productos vendidos en nuestra tienda en línea. 
+--  -> LEFT JOIN para analizar pedidos y usuarios
+select u.nombre, u.apellido, p.fecha_pedido, p.total_pedido
+from Usuarios u
+LEFT JOIN Pedidos p
+	ON u.user_id = p.user_id
+    where p.fecha_pedido is null;
+
+--  -> RIGHT JOIN para analizar pedidos y productos
+select p.nombre_producto, dp.cantidad, dp.precio_unitario
+from Productos p
+RIGHT JOIN Detalles_Pedido dp
+	ON p.producto_id = dp.producto_id;
+
+
+
