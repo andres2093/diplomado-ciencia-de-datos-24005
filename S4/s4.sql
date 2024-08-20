62cd02237479bda3ecb9b0c7b17114f2f86d10cf
Andrés Ramírez <andres200493@gmail.com>
2024-08-19 19:33:15 -0600

    S4 -> R1

--- S4/s4.sql
+++ S4/s4.sql
@@ -11,3 +11,20 @@ JOIN Pedidos p
 JOIN Detalles_Pedidos dp
 ON u.user_id = p.user_id
 ON p.user_id = dp.user_id;
+
+-- ///////////////// RETO 01 /////////////////
+-- Realiza un análisis de los productos comprados por cada usuario
+SELECT u.nombre, pr.nombre_producto, p.pedido_id
+FROM Detalles_Pedido dp
+JOIN Pedidos p
+  ON dp.pedido_id = p.pedido_id
+JOIN Productos pr
+  ON dp.producto_id = pr.producto_id
+JOIN Usuarios u
+  ON p.user_id = u.user_id
+--  where p.user_id = 10000 -- Comprobar
+ORDER BY u.nombre;
+-- Comprobar
+select * from Detalles_Pedido where pedido_id in(
+	select pedido_id from Pedidos where user_id = 10000
+);
