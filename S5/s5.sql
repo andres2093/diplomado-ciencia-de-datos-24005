8179b2cea0dc5227deef25d1ae420c8c0ab78559
Andrés Ramírez <andres200493@gmail.com>
2024-08-22 09:58:58 -0600

    S5 -> fix

--- S5/s5.sql
+++ S5/s5.sql
@@ -45,6 +45,98 @@ INSERT INTO Usuarios(nombre, apellido, edad, correo_electronico, fecha_registro)
 VALUES ("Juan", "Pérez", 30, "juan@gamil.com", "2024-08-15");
 INSERT INTO Usuarios(nombre, apellido, edad, correo_electronico, fecha_registro)
 VALUES ("Jose", "Lopez", 35, "jose@gmail.com", "2024-08-16");
+INSERT INTO Usuarios(nombre)
+VALUES ("Lore");
 -- Consultar usuarios
 SELECT * FROM Usuarios;
 
+-- ///////////////// RETO 02 /////////////////
+CREATE TABLE Alumnos (
+    matricula VARCHAR(11) PRIMARY KEY,
+    nombre VARCHAR(45),
+    apellido VARCHAR(45)
+);
+CREATE TABLE Tareas (
+   id_tarea INT PRIMARY KEY,
+   matricula VARCHAR(11),
+   numero_tarea INT,
+   calificacion DOUBLE,
+   FOREIGN KEY (matricula) REFERENCES Alumnos(matricula)
+);
+CREATE TABLE Examenes (
+  id_examen INT PRIMARY KEY,
+   matricula VARCHAR(11),
+   numero_examen INT,
+   calificacion DOUBLE,
+   FOREIGN KEY (matricula) REFERENCES Alumnos(matricula)
+);
+-- ///////////////// RETO 03 /////////////////
+-- Consultar usuarios
+SELECT * FROM Alumnos;
+SELECT * FROM Tareas;
+SELECT * FROM Examenes;
+-- ///////////////// RETO 04 /////////////////
+-- Necesitamos tener los datos de los alumnos, las tareas y exámenes en una sola tabla
+select * from Alumnos a
+join Tareas t on a.matricula = t.matricula
+join Examenes e on a.matricula = e.matricula;
+-- Mostrar la info como estaba en el excel sin promedio
+SELECT a.matricula, a.nombre, a.apellido,
+	SUM(CASE WHEN e.numero_examen = 1 THEN e.calificacion ELSE 0 END) AS examen_1,
+	SUM(CASE WHEN e.numero_examen = 2 THEN e.calificacion ELSE 0 END) AS examen_2,
+	SUM(CASE WHEN t.numero_tarea = 1 THEN t.calificacion ELSE 0 END) AS tarea_1,
+	SUM(CASE WHEN t.numero_tarea = 2 THEN t.calificacion ELSE 0 END) AS tarea_2
+FROM Alumnos a
+JOIN Tareas t ON a.matricula = t.matricula
+JOIN Examenes e ON a.matricula = e.matricula
+GROUP BY a.matricula, a.nombre, a.apellido;
+-- Mostrar la info como estaba en el excel con promedios
+SELECT tareas.matricula, tareas.nombre, tareas.apellido, examen_1, examen_2,
+      (examen_1 + examen_2) / 2 AS promedio_examenes,
+      tarea_1,tarea_2,
+      (tarea_1 + tarea_2) / 2 AS promedio_tareas,
+      ((examen_1 + examen_2) / 2)*0.6 + ((tarea_1 + tarea_2) / 2 ) * 0.4 AS promedio_final
+FROM (
+	SELECT a.matricula, a.nombre, a.apellido,
+		  SUM(CASE WHEN t.numero_tarea = 1 THEN t.calificacion ELSE 0 END) AS tarea_1,
+		  SUM(CASE WHEN t.numero_tarea = 2 THEN t.calificacion ELSE 0 END) AS tarea_2
+	FROM Alumnos a
+	JOIN Tareas t ON a.matricula = t.matricula
+	GROUP BY a.matricula, a.nombre, a.apellido
+) AS tareas
+JOIN (
+	SELECT a.matricula, a.nombre, a.apellido,
+		  SUM(CASE WHEN e.numero_examen = 1 THEN e.calificacion ELSE 0 END) AS examen_1,
+		  SUM(CASE WHEN e.numero_examen = 2 THEN e.calificacion ELSE 0 END) AS examen_2
+	FROM Alumnos a
+	JOIN Examenes e ON a.matricula = e.matricula
+	GROUP BY a.matricula, a.nombre, a.apellido
+) AS examenes ON tareas.matricula = examenes.matricula
+ORDER BY tareas.matricula;
+-- Crear una vista a partir de un query
+CREATE VIEW Calificaciones AS (
+	SELECT tareas.matricula, tareas.nombre, tareas.apellido, examen_1, examen_2,
+	  (examen_1 + examen_2) / 2 AS promedio_examenes,
+	  tarea_1,tarea_2,
+	  (tarea_1 + tarea_2) / 2 AS promedio_tareas,
+	  ((examen_1 + examen_2) / 2)*0.6 + ((tarea_1 + tarea_2) / 2 ) * 0.4 AS promedio_final
+	FROM (
+		SELECT a.matricula, a.nombre, a.apellido,
+			  SUM(CASE WHEN t.numero_tarea = 1 THEN t.calificacion ELSE 0 END) AS tarea_1,
+			  SUM(CASE WHEN t.numero_tarea = 2 THEN t.calificacion ELSE 0 END) AS tarea_2
+		FROM Alumnos a
+		JOIN Tareas t ON a.matricula = t.matricula
+		GROUP BY a.matricula, a.nombre, a.apellido
+	) AS tareas
+	JOIN (
+		SELECT a.matricula, a.nombre, a.apellido,
+			  SUM(CASE WHEN e.numero_examen = 1 THEN e.calificacion ELSE 0 END) AS examen_1,
+			  SUM(CASE WHEN e.numero_examen = 2 THEN e.calificacion ELSE 0 END) AS examen_2
+		FROM Alumnos a
+		JOIN Examenes e ON a.matricula = e.matricula
+		GROUP BY a.matricula, a.nombre, a.apellido
+	) AS examenes ON tareas.matricula = examenes.matricula
+	ORDER BY tareas.matricula
+);
+-- Ejecutar vista
+Select * from Calificaciones where promedio_final > 6 order by promedio_final desc;
